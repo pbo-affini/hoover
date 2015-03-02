fec05ca0df024f1d64f0708a3eda8ed22e155f84
Pierrick Boitel <pbo@affini-tech.com>
2015-03-02 16:44:58 +0100

    Finish functionalities and add all unit tests

--- hoover.py
+++ hoover.py
@@ -1,85 +1,170 @@
+#! venv/bin/python
+
 import csv
 import os
 import copy
 import multiprocessing
+import json
+import inspect
 
 TYPE_DICT = {1 : "integer", 2 : "float", 3 : "string"}
 
-def create_transformation_function(expression, type):
+def convert_to_type(x, type):
+    if type == "integer":
+        x = int(x)
+    if type == "float":
+        x = float(x)
+    return x
+
+def create_transformation_function(expression):
 
     def transformation(x):
-        if type == "integer":
-            x = int(x)
-        if type == "float":
-            x = float(x)
-        result = eval(expression)
+        try:
+            result = eval(expression)
+        except NameError:
+            raise NameError
         return result
 
     return transformation
 
 class Hoover(object):
 
-    def __init__(self, filetoclean, delimiter, filetowrite,nb_process):
+    def __init__(self, filetoclean, delimiter, filetowrite, nb_process=1):
         self.filetoclean = filetoclean
         self.delimiter = delimiter
         self.filetowrite = filetowrite
         self.nb_process = nb_process
 
-    def add_type(self):
-        type_list = []
-        with open(self.filetoclean) as input_file:
-            reader = csv.reader(input_file, delimiter=self.delimiter)
-            firstline = reader.next()
-        self.firstline = firstline
-        for key, type in TYPE_DICT.items():
-            print "%s : %s" %(key, type)
-        print "For each field, type the type of the field"
-        for field in firstline:
-            while True:
-                print(str(field) + " : "),
-                try:
-                    type_list.append(TYPE_DICT[input()])
-                    break
-                except KeyError:
-                    print "Invalid number. Please retry"
-                except NameError:
-                    print "Invalid name. Please retry"
-        self.type_list = type_list
-
-    def add_transformation(self):
-        transformation_list = []
-        print "For each field x, print the transformation to apply to x"
-        for i, field in enumerate(self.firstline):
-            while True:
-                print(str(field) + " : "),
-                transformation_list.append(create_transformation_function(raw_input(), self.type_list[i]))
-                try:
-                    result = transformation_list[i](field)
-                    print "The result will for this field will be %s, is this correct ? [y/n]" %(result)
+    def add_header(self, has_header, header_file=False):
+        print "Addition of headers"
+        if header_file:
+            with open(self.filetoclean + "_header") as inputfile:
+                self.header = json.load(inputfile)
+            with open(self.filetoclean + "_first_line") as inputfile:
+                self.first_line = json.load(inputfile)
+        else:
+            with open(self.filetoclean) as input_file:
+                first_line = input_file.next()
+                if has_header:
+                    header = first_line.replace('\n','').split(self.delimiter)
+                    with open(self.filetoclean + "_copy", "w") as output_file:
+                        first_line = input_file.next()
+                        output_file.write(first_line)
+                        for line in input_file:
+                            output_file.write(line)
+                    os.rename(self.filetoclean + "_copy",self.filetoclean)
+                else:
+                    header = []
+                    print "Do you want to add column name ? [y/n]"
                     agree = raw_input()
                     if agree == "y":
+                        print "For each field, print the column name"
+                        for i, field in enumerate(first_line.split(self.delimiter)):
+                            print("Field " + str(i) + " : "),
+                            header.append(raw_input())
+                    else:
+                        header = ["field " + str(i) for i in range(0, len(first_line.split(self.delimiter)))]
+                self.header = header
+                self.first_line = first_line.replace('\n','').split(self.delimiter)
+
+    def add_type(self, list_type_file=False):
+        print "Addition of types"
+        if list_type_file:
+            with open(self.filetoclean + "_type_list") as inputfile:
+                self.type_list = json.load(inputfile)
+        else:
+            type_list = []
+            for key, type in TYPE_DICT.items():
+                print "%s : %s" %(key, type)
+            print "For each field, type the type of the field"
+            for field in self.header:
+                while True:
+                    print(str(field) + " : "),
+                    try:
+                        type_list.append(TYPE_DICT[input()])
                         break
-                    transformation_list.pop()
-                except SyntaxError:
-                    print "The function you write has an invalid syntax. Please retry"
-                    transformation_list.pop()
+                    except KeyError:
+                        print "Invalid number. Please retry"
+                    except NameError:
+                        print "Invalid name. Please retry"
+            self.type_list = type_list
+
+    def add_transformation(self, functions_dict=None, field_to_transform=None,file_transformation=False):
+        print "Addition of transformations"
+        transformation_list = []
+        transformation_code = []
+        if file_transformation:
+            with open(self.filetoclean + "_transformation_code") as inputfile:
+                for i, code in enumerate(json.load(inputfile)):
+                    transformation_code.append(code)
+                    print self.type_list
+                    print str(i)
+                    if "def" in code:
+                        exec(code)
+                        func_name = code.split(' ')[1].split('(')[0]
+                        transformation_list.append(eval(func_name))
+                    else:
+                        transformation_list.append(create_transformation_function(code))
+        else:
+            print "For each field x, print the transformation to apply to x"
+            for i, field in enumerate(self.header):
+                if field_to_transform and i not in field_to_transform:
+                    transformation_list.append(create_transformation_function("x"))
+                    continue
+                while True:
+                    print(str(field) + " : "),
+                    function = raw_input()
+                    if functions_dict and function in functions_dict:
+                        transformation_list.append(functions_dict[function])
+                        transformation_code.append("".join(inspect.getsourcelines(functions_dict[function])[0]))
+                    else:
+                        transformation_list.append(create_transformation_function(function))
+                        transformation_code.append(function)
+                    try:
+                        result = transformation_list[i](convert_to_type(self.first_line[i], self.type_list[i]))
+                        print "The result will for this field for the value %s will be %s, is this correct ? [y/n]" %(self.first_line[i],result)
+                        agree = raw_input()
+                        if agree == "y":
+                            break
+                        transformation_list.pop()
+                        transformation_code.pop()
+                    except SyntaxError:
+                        print "The function you write has an invalid syntax. Please retry"
+                        transformation_list.pop()
+                        transformation_code.pop()
+                    except NameError:
+                        print "The function you write has an invalid name. Please retry"
+                        transformation_code.pop()
+                        transformation_list.pop()
+        self.transformation_code = transformation_code
         self.transformation_list = transformation_list
 
     def apply_line_transformation(self, line):
         line_result = []
         for i, field in enumerate(line):
-            line_result.append(self.transformation_list[i](field))
+            line_result.append(self.transformation_list[i](convert_to_type(field, self.type_list[i])))
         return line_result
 
     def apply_file_transformation(self):
+        print "Begin the file transformation"
+        error_exist = False
         with open(self.filetoclean) as input_file:
             with open(self.filetowrite,"w") as output_file:
-                reader = csv.reader(input_file, delimiter = self.delimiter)
-                writer = csv.writer(output_file, delimiter = self.delimiter)
-                for line in reader:
-                    writer.writerow(self.apply_line_transformation(line))
+                with open(self.filetowrite + "_error","w") as output_file_error:
+                    reader = csv.reader(input_file, delimiter = self.delimiter)
+                    writer = csv.writer(output_file, delimiter = self.delimiter, lineterminator="\n")
+                    writer_error = csv.writer(output_file_error, delimiter = self.delimiter, lineterminator="\n")
+                    for line in reader:
+                        try:
+                            writer.writerow(self.apply_line_transformation(line))
+                        except:
+                            error_exist = True
+                            writer_error.writerow(line)
+        if not error_exist:
+            os.remove(self.filetowrite + "_error")
 
     def separate_into_chunks(self):
+        print "Begin the transformation into chunks"
         nb_lines = sum(1 for line in open(self.filetoclean))
         nb_lines_by_chunk = nb_lines/self.nb_process
         with open(self.filetoclean) as input_file:
@@ -96,6 +181,7 @@ class Hoover(object):
                             outputfile.write(line)
 
     def apply_file_transformation_multiprocess(self):
+        print "Begin the transformation in multiprocess"
         list_process = []
         for i in range(0, self.nb_process):
             c = copy.copy(self)
@@ -108,6 +194,7 @@ class Hoover(object):
             p.join()
 
     def reassemblate_chunks(self):
+        print "Begin the assemmbly of chunks"
         with open(self.filetowrite, "w") as output_file:
             for i in range(0, self.nb_process):
                 with open(self.filetowrite + "_" + str(i)) as input_file:
@@ -115,14 +202,52 @@ class Hoover(object):
                         output_file.write(line)
                 os.remove(self.filetoclean + "_" + str(i))
                 os.remove(self.filetowrite + "_" + str(i))
+        error_exist = False
+        with open(self.filetowrite + "_error", "w") as error_file:
+            for i in range(0, self.nb_process):
+                if os.path.exists(self.filetowrite + "_" + str(i) + "_error"):
+                    error_exist = True
+                    with open(self.filetowrite + "_" + str(i) + "_error") as chunk_error_file:
+                        for line in chunk_error_file:
+                            error_file.write(line)
+                    os.remove(self.filetowrite + "_" + str(i) + "_error")
+        if not error_exist:
+            os.remove(self.filetowrite + "_error")
+    
+    def write_infos(self, header, first_line, type_list, transformation_list):
+        print "Begin the write of infos"
+        if header:
+            with open(self.filetoclean + "_header","w") as header_file:
+                json.dump(self.header,header_file)
+        if first_line:
+            with open(self.filetoclean + "_first_line","w") as first_line_file:
+                json.dump(self.first_line,first_line_file)
+        if type_list:
+            with open(self.filetoclean + "_type_list","w") as type_file:
+                json.dump(self.type_list, type_file)
+        if transformation_list:
+            #transformation_code = ["".join(inspect.getsourcelines(func)[0]).replace('    def','def').replace('\n        ','\n    ') for func in self.transformation_list]
+            with open(self.filetoclean + "_transformation_code","w") as transformation_file:
+                json.dump(self.transformation_code, transformation_file)
+
+
+    def write_with_header(self):
+        print "Begin the wirte with headers"
+        with open(self.filetowrite) as input_file:
+            with open(self.filetowrite + "_copy", "w") as outputfile:
+                outputfile.write(self.delimiter.join(self.header) + "\n")
+                for line in input_file:
+                    outputfile.write(line)
+        os.rename(self.filetowrite + "_copy",self.filetowrite)
 
 if __name__ == "__main__":
-    ch = Hoover("test.csv",",","output.csv", 2)
+    ch = Hoover("test_file/test.csv",",","test_file/output.csv", 1)
+    ch.add_header(True)
     ch.add_type()
     #print ch.type_dict
     ch.add_transformation()
     #print ch.apply_line_transformation(ch.firstline)
-    #ch.apply_file_transformation()
-    ch.separate_into_chunks()
-    ch.apply_file_transformation_multiprocess()
-    ch.reassemblate_chunks()
+    ch.apply_file_transformation()
+    #ch.separate_into_chunks()
+    #ch.apply_file_transformation_multiprocess()
+    #ch.reassemblate_chunks()
---
+++ test_hoover.py
@@ -0,0 +1,284 @@
+from hoover import *
+import pytest
+import mock
+import os
+
+@pytest.fixture()
+def hoover():
+    hoover = Hoover("test_file/input.csv", ",", "test_file/output.csv")
+    return hoover
+
+@pytest.fixture()
+def hoover_multi():
+    hoover = Hoover("test_file/input.csv", ",", "test_file/output.csv", 3)
+    with mock.patch('__builtin__.raw_input', return_value='n'):
+        hoover.add_header(False)
+    with mock.patch('__builtin__.input', side_effect=[3, 1, 3, 1]):
+        hoover.add_type()
+    with mock.patch('__builtin__.raw_input', side_effect = ["x + \'test\'","y", "x * 2", "y"]):
+        hoover.add_transformation(field_to_transform=[0, 1])
+    return hoover
+
+@pytest.yield_fixture()
+def hoover_header():
+    hoover = Hoover("test_file/input_with_header.csv", ",", "test_file/outputi_with_header.csv")
+    yield hoover
+    with open("test_file/input_with_header.csv", "w") as outputfile:
+        outputfile.write("firstheader,secondheader\n1,2\n3,4")
+
+@pytest.yield_fixture()
+def hoover_complete():
+    functions_dict = {"multiply()" : multiply}
+    hoover = Hoover("test_file/input_with_header.csv", ",", "test_file/output_with_header.csv")
+    hoover.add_header(True)
+    with mock.patch('__builtin__.input', side_effect=[1 , 3]):
+        hoover.add_type()
+    with mock.patch('__builtin__.raw_input', side_effect = ["multiply()", "y", "x + '_test'", "y"]):
+        hoover.add_transformation(functions_dict)
+    yield hoover
+    with open("test_file/input_with_header.csv", "w") as outputfile:
+        outputfile.write("firstheader,secondheader\n1,2\n3,4")
+
+@pytest.yield_fixture()
+def hoover_error():
+    functions_dict = {"multiply()" : multiply}
+    hoover = Hoover("test_file/input_with_error.csv", ",", "test_file/output_with_error.csv")
+    with mock.patch('__builtin__.raw_input', return_value='n'):
+        hoover.add_header(False)
+    with mock.patch('__builtin__.input', side_effect=[1 , 3]):
+        hoover.add_type()
+    with mock.patch('__builtin__.raw_input', side_effect = ["multiply()", "y", "x + '_test'", "y"]):
+        hoover.add_transformation(functions_dict)
+    yield hoover
+
+def test_add_header(hoover_header, hoover):
+    hoover_header.add_header(True)
+    assert hoover_header.header == ["firstheader","secondheader"]
+    assert hoover_header.first_line == ["1","2"]
+    
+    with mock.patch('__builtin__.raw_input', return_value='n'):
+        hoover.add_header(False)
+        assert hoover.header == ["field 0","field 1", "field 2", "field 3"]
+    assert hoover.first_line == ["t1","1","f1","1"]
+    hoover.header = None
+
+    with mock.patch('__builtin__.raw_input', side_effect=["y","f0","f1","f2","f3"]):
+        hoover.add_header(False)
+        assert hoover.header == ["f0","f1", "f2", "f3"]
+    assert hoover.first_line == ["t1","1","f1","1"]
+    hoover.write_infos(True, True, False, False)
+    hoover.header = None
+    hoover.first_line = None
+
+    hoover.add_header(False,True)
+    assert hoover.header == ["f0","f1", "f2", "f3"]
+    assert hoover.first_line == ["t1","1","f1","1"]
+    os.remove("test_file/input.csv_header")
+    os.remove("test_file/input.csv_first_line")
+
+def test_add_type(hoover_header):
+    hoover_header.add_header(True)
+    with mock.patch('__builtin__.input', side_effect=[1 , 3]):
+        hoover_header.add_type()
+        assert hoover_header.type_list == ["integer", "string"]
+    hoover_header.write_infos(False, False, True, False)
+    hoover_header.type_list = []
+    hoover_header.add_type(True)
+    assert hoover_header.type_list == ["integer", "string"]
+    os.remove("test_file/input_with_header.csv_type_list")
+
+def multiply(x):
+    return x * 2
+
+def test_add_transformation(hoover):
+    functions_dict = {"multiply()" : multiply}
+    with mock.patch('__builtin__.raw_input', side_effect = ["y","f0","f1","f2","f3"]):
+        hoover.add_header(False)
+    with mock.patch('__builtin__.input', side_effect=[3 , 1, 3, 1]):
+        hoover.add_type()
+
+    #test with a functions dict
+    with mock.patch('__builtin__.raw_input', side_effect = ["x + \'test\'","y","multiply()","y","yexf","x + \'_test2\'","n","x","y","x","y"]):
+        hoover.add_transformation(functions_dict)
+    assert hoover.transformation_list[0]("field") == "fieldtest"
+    assert hoover.transformation_list[1](2) == 4
+    assert hoover.transformation_list[2]("field") == "field"
+    assert hoover.transformation_list[3](2) == 2
+
+    #test with file load
+    hoover.write_infos(False, False, False, True)
+    hoover.transformation_list = []
+    hoover.add_transformation(file_transformation = True)
+    print hoover.transformation_list[0]("field")
+    assert hoover.transformation_list[0]("field") == "fieldtest"
+    assert hoover.transformation_list[1](2) == 4
+    assert hoover.transformation_list[2]("field") == "field"
+    assert hoover.transformation_list[3](2) == 2
+    os.remove("test_file/input.csv_transformation_code")
+
+    #test without functions dict and with selected field
+    hoover.transformation_list = []
+    with mock.patch('__builtin__.raw_input', side_effect = ["x + \'test\'","y"]):
+        hoover.add_transformation(field_to_transform=[0])
+    assert hoover.transformation_list[0]("field") == "fieldtest"
+    assert hoover.transformation_list[1](2) == 2
+    assert hoover.transformation_list[2]("field") == "field"
+    assert hoover.transformation_list[3](2) == 2
+
+def test_apply_line_transformation(hoover_complete):
+    line = [2 , "test"]
+    assert hoover_complete.apply_line_transformation(line) == [4, "test_test"]
+
+def test_apply_file_transformation(hoover_complete, hoover_error):
+    hoover_complete.apply_file_transformation()
+    assert os.path.exists("test_file/output_with_header.csv")
+    assert not os.path.exists("test_file/output_with_header.csv_error")
+    with open("test_file/output_with_header.csv") as output_file:
+        lines = output_file.readlines()
+        assert len(lines) == 2
+        assert lines[0] == "2,2_test\n"
+        assert lines[1] == "6,4_test\n"
+    os.remove("test_file/output_with_header.csv")
+
+    #test with errors
+    hoover_error.apply_file_transformation()
+    assert os.path.exists("test_file/output_with_error.csv")
+    with open("test_file/output_with_error.csv") as output_file:
+        lines = output_file.readlines()
+        assert len(lines) == 3
+        assert lines[0] == "2,2_test\n"
+        assert lines[1] == "10,6_test\n"
+        assert lines[2] == "14,8_test\n"
+    assert os.path.exists("test_file/output_with_error.csv_error")
+    with open("test_file/output_with_error.csv_error") as error_file:
+        lines = error_file.readlines()
+        assert len(lines) == 2
+        assert lines[0] == "a,4\n"
+        assert lines[1] == "b,10\n"
+    os.remove("test_file/output_with_error.csv")
+    os.remove("test_file/output_with_error.csv_error")
+
+def test_separate_into_chunks(hoover_multi):
+    hoover_multi.separate_into_chunks()
+    assert os.path.exists("test_file/input.csv_0")
+    assert os.path.exists("test_file/input.csv_1")
+    assert os.path.exists("test_file/input.csv_2")
+    with open("test_file/input.csv_0") as input_file:
+        lines = input_file.readlines()
+        assert len(lines) == 3
+        assert lines[0] == "t1,1,f1,1\n"
+        assert lines[1] == "t2,2,f2,2\n"
+        assert lines[2] == "t2,a,f2,2\n"
+    with open("test_file/input.csv_1") as input_file:
+        lines = input_file.readlines()
+        assert len(lines) == 3
+        assert lines[0] == "t2,4,f2,2\n"
+        assert lines[1] == "t2,5,f2,2\n"
+        assert lines[2] == "t2,6,f2,2\n"
+    with open("test_file/input.csv_2") as input_file:
+        lines = input_file.readlines()
+        assert len(lines) == 4
+        assert lines[0] == "t2,7,f2,2\n"
+        assert lines[1] == "t2,8,f2,2\n"
+        assert lines[2] == "t2,c,f2,2\n"
+        assert lines[3] == "t2,10,f2,2\n"
+    os.remove("test_file/input.csv_0")
+    os.remove("test_file/input.csv_1")
+    os.remove("test_file/input.csv_2")
+
+def test_apply_file_transformation_multiprocess(hoover_multi):
+    hoover_multi.separate_into_chunks()
+    hoover_multi.apply_file_transformation_multiprocess()
+    assert os.path.exists("test_file/output.csv_0")
+    assert os.path.exists("test_file/output.csv_1")
+    assert os.path.exists("test_file/output.csv_2")
+    assert os.path.exists("test_file/output.csv_0_error")
+    assert os.path.exists("test_file/output.csv_2_error")
+    with open("test_file/output.csv_0") as input_file:
+        lines = input_file.readlines()
+        assert len(lines) == 2
+        assert lines[0] == "t1test,2,f1,1\n"
+        assert lines[1] == "t2test,4,f2,2\n"
+    with open("test_file/output.csv_1") as input_file:
+        lines = input_file.readlines()
+        assert len(lines) == 3
+        assert lines[0] == "t2test,8,f2,2\n"
+        assert lines[1] == "t2test,10,f2,2\n"
+        assert lines[2] == "t2test,12,f2,2\n"
+    with open("test_file/output.csv_2") as input_file:
+        lines = input_file.readlines()
+        assert len(lines) == 3
+        assert lines[0] == "t2test,14,f2,2\n"
+        assert lines[1] == "t2test,16,f2,2\n"
+        assert lines[2] == "t2test,20,f2,2\n"
+    os.remove("test_file/input.csv_0")
+    os.remove("test_file/input.csv_1")
+    os.remove("test_file/input.csv_2")
+    os.remove("test_file/output.csv_0")
+    os.remove("test_file/output.csv_1")
+    os.remove("test_file/output.csv_2")
+    os.remove("test_file/output.csv_0_error")
+    os.remove("test_file/output.csv_2_error")
+
+
+def test_reassemblate_chunks(hoover_multi):
+    hoover_multi.separate_into_chunks()
+    hoover_multi.apply_file_transformation_multiprocess()
+    hoover_multi.reassemblate_chunks()
+    assert not os.path.exists("test_file/input.csv_0")
+    assert not os.path.exists("test_file/output.csv_0")
+    assert not os.path.exists("test_file/output.csv_0_error")
+    assert os.path.exists("test_file/output.csv")
+    assert os.path.exists("test_file/output.csv_error")
+    with open("test_file/output.csv") as output_file:
+        lines = output_file.readlines()
+        assert len(lines) == 8
+        assert lines[0] == "t1test,2,f1,1\n"
+        assert lines[1] == "t2test,4,f2,2\n"
+        assert lines[2] == "t2test,8,f2,2\n"
+        assert lines[3] == "t2test,10,f2,2\n"
+        assert lines[4] == "t2test,12,f2,2\n"
+        assert lines[5] == "t2test,14,f2,2\n"
+        assert lines[6] == "t2test,16,f2,2\n"
+        assert lines[7] == "t2test,20,f2,2\n"
+    with open("test_file/output.csv_error") as error_file:
+        lines = error_file.readlines()
+        assert len(lines) == 2
+        assert lines[0] == "t2,a,f2,2\n"
+        assert lines[1] == "t2,c,f2,2\n"
+    os.remove("test_file/output.csv")
+    os.remove("test_file/output.csv_error")
+
+def test_write_infos(hoover_complete):
+    functions_dict = {"multiply()" : multiply}
+    hoover_complete.write_infos(True, True, True, True)
+    assert os.path.exists("test_file/input_with_header.csv_header")
+    assert os.path.exists("test_file/input_with_header.csv_first_line")
+    assert os.path.exists("test_file/input_with_header.csv_type_list")
+    assert os.path.exists("test_file/input_with_header.csv_transformation_code")
+    with open("test_file/input_with_header.csv_header") as header_file:
+        assert json.load(header_file) == ["firstheader","secondheader"]
+    with open("test_file/input_with_header.csv_first_line") as first_line_file:
+        assert json.load(first_line_file) == ["1", "2"]
+    with open("test_file/input_with_header.csv_type_list") as type_list_file:
+        assert json.load(type_list_file) == ["integer", "string"]
+    with open("test_file/input_with_header.csv_transformation_code") as transformation_list_file:
+        assert json.load(transformation_list_file) == ["def multiply(x):\n    return x * 2\n", "x + '_test'"]
+    os.remove ("test_file/input_with_header.csv_header")
+    os.remove("test_file/input_with_header.csv_first_line")
+    os.remove("test_file/input_with_header.csv_type_list")
+    os.remove("test_file/input_with_header.csv_transformation_code")
+
+def test_write_with_header(hoover_complete):
+    hoover_complete.apply_file_transformation()
+    hoover_complete.write_with_header()
+    assert os.path.exists(hoover_complete.filetowrite)
+    with open(hoover_complete.filetowrite) as output_file:
+        lines = output_file.readlines()
+        assert len(lines) == 3
+        assert lines[0] == "firstheader,secondheader\n"
+        assert lines[1] == "2,2_test\n"
+        assert lines[2] == "6,4_test\n"
+    os.remove(hoover_complete.filetowrite)
+
+
